ed79a5b6fdc878e41e544bda0b48846b9021398e
Jazmin Morrison <home-morrison@jazmins.macbook.air.lan>
2026-07-03 19:09:05 -0500

    Fix static Cloudflare build

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,10 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   buildDirectory: "dist",
+  ssr: false,
+} satisfies Config;import type { Config } from "@react-router/dev/config";
+
+export default {
+  buildDirectory: "dist",
   ssr: true,
 } satisfies Config;
