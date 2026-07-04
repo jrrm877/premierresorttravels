5ac1dc81e2393c04d8ed8edbf6bd8db0c48a282b
Jazmin Morrison <home-morrison@jazmins.macbook.air.lan>
2026-07-03 19:11:27 -0500

    Fix duplicate React Router config export

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,9 +3,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
   buildDirectory: "dist",
   ssr: false,
-} satisfies Config;import type { Config } from "@react-router/dev/config";
-
-export default {
-  buildDirectory: "dist",
-  ssr: true,
 } satisfies Config;
